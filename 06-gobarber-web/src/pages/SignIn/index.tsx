859b93b74079788cd533b23340a808c3e6625f2a
lucasdavisan <Lucas Davi>
2020-07-22 21:21:33 -0300

    Isolating components.

--- 06-gobarber-web/src/pages/SignIn/index.tsx
+++ 06-gobarber-web/src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -14,16 +17,20 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu login</h1>
 
-        <input placeholder="E-mail" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <input type="password" placeholder="Senha" />
-
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
-      <a href="">
+      <a href="login">
         <FiLogIn />
         Criar conta
       </a>
