25ea5310cf7fb2471ea7d5e6d97230defc968c52
lucasdavisan <Lucas Davi>
2020-07-25 11:20:58 -0300

    Context API.

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -4,9 +4,14 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Diego' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
